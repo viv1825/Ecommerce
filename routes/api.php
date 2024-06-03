e7c36fc673046597c60cc847245d2a72758e1b9b
Vivek jha <jhavivek1800@gmail.com>
2024-06-03 09:41:30 +0530

    final commit

--- routes/api.php
+++ routes/api.php
@@ -8,31 +8,26 @@ use App\Http\Controllers\ProductController;
 
 Route::apiResource('products',ProductController::class);
 
-// Get a list of products, with pagination
-// Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-// Description: Retrieves a paginated list of all products.
-// Example: GET http://localhost:8000/api/products
+// Method: GET
+// path: http://localhost:8000/api/products
+// Retrieves a paginated list of all products.
     
 
-// Store a newly created product
-// Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-// Description: Stores a newly created product in the database.
-// Example: POST http://localhost:8000/api/products
+// Method: POST
+// path:  http://localhost:8000/api/products
+// Stores new product in the database.
 
 
-// Get details of a specific product by its ID
-// Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
-// Description: Retrieves the details of a specific product by its ID.
-// Example: GET http://localhost:8000/api/products/1
+// Method: GET
+// path:  http://localhost:8000/api/products/1
+// Retrieves the details of a specific product by its ID.
 
 
-// Update a specific product by its ID
-// Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-// Description: Updates the details of a specific product by its ID.
-// Example: PUT http://localhost:8000/api/products/1
+// Method: PUT
+// path:  http://localhost:8000/api/products/1
+// Updates the details of a specific product by its ID.
 
 
-// Delete a specific product by its ID
-// Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
-// Description: Deletes a specific product by its ID.
-// Example: DELETE http://localhost:8000/api/products/1
+// Method: DELETE
+// path: http://localhost:8000/api/products/1
+// Deletes a specific product by its ID.
